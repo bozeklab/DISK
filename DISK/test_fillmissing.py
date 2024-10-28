e6a0029b8298b4b739930ddb117e6c3eb87bf7cf
FranceRose <france.rose@wanadoo.fr>
2024-10-28 12:54:00 +0100

    fix bug when add_missing not in config

--- DISK/test_fillmissing.py
+++ DISK/test_fillmissing.py
@@ -307,7 +307,7 @@ def evaluate(_cfg: DictConfig) -> None:
                 """VISUALIZATION, only first batch"""
                 if n_plots < _cfg.evaluate.n_plots:
                     logging.info(f'Starting sample plots')
-                    potential_indices = np.where(n_missing > 0)[0]
+                    potential_indices = np.where(n_missing > 0)[0] if ('add_missing' not in _cfg.feed_data.transforms.keys()) else np.arange(len(n_missing))
                     np.random.seed(0)
                     for i in np.random.choice(potential_indices,  #full_data_np.shape[0],
                                               min(len(potential_indices), _cfg.evaluate.n_plots),
