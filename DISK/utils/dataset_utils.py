e5eb80e5dbe7254a06ca192f8538d38f3996ae03
FranceRose <france.rose@wanadoo.fr>
2025-03-13 13:46:31 +0900

    debug

--- DISK/utils/dataset_utils.py
+++ DISK/utils/dataset_utils.py
@@ -81,10 +81,10 @@ class ParentDataset(data.Dataset):
                          f'{np.any(np.all(np.isnan(self.X[..., 0]), axis=2), axis=1).shape} {np.any(np.all(np.isnan(self.X[..., 0]), axis=2), axis=1)}')
 
             mask_all_nans = np.any(np.all(np.isnan(self.X[..., 0]), axis=2), axis=1)
-            mask_0_nans = np.any(~np.all(np.isnan(self.X[..., 0]), axis=2), axis=1)
+            mask_0_nans = np.any(np.all(~np.isnan(self.X[..., 0]), axis=2), axis=1)
             self.X = self.X[(~mask_all_nans) * (~mask_0_nans)]
             self.X_gt = self.X_gt[(~mask_all_nans) * (~mask_0_nans)]
-            logging.info(f'[AFTER MASKING] {self.X.shape} {self.X_gt.shape}')
+            logging.info(f'[AFTER MASKING] {self.X.shape} {self.X_gt.shape} {mask_0_nans}')
 
         else:
             self.X = self.data_dict['X']  # shape (batch, max_len, features)
