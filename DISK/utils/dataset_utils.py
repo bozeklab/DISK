899977b3a65f5f69d664838cfa2a818bac983452
FranceRose <france.rose@wanadoo.fr>
2025-03-16 11:03:33 +0900

    first timestep not all nans mask

--- DISK/utils/dataset_utils.py
+++ DISK/utils/dataset_utils.py
@@ -82,9 +82,11 @@ class ParentDataset(data.Dataset):
 
             mask_all_nans = np.any(np.all(np.isnan(self.X[..., 0]), axis=2), axis=1)
             mask_0_nans = np.any(np.all(~np.isnan(self.X[..., 0]), axis=2), axis=1)
-            self.X = self.X[(~mask_all_nans) * (~mask_0_nans)]
-            self.X_gt = self.X_gt[(~mask_all_nans) * (~mask_0_nans)]
-            logging.info(f'[AFTER MASKING] {self.X.shape} {self.X_gt.shape} {mask_0_nans}')
+            mask_nan_first_position = np.isnan(self.X[:, 0, ..., 0])
+
+            self.X = self.X[(~mask_all_nans) * (~mask_0_nans) * (~mask_nan_first_position)]
+            self.X_gt = self.X_gt[(~mask_all_nans) * (~mask_0_nans) * (~mask_nan_first_position)]
+            logging.info(f'[AFTER MASKING] {self.X.shape} {self.X_gt.shape} {mask_0_nans} {mask_nan_first_position}')
 
         else:
             self.X = self.data_dict['X']  # shape (batch, max_len, features)
