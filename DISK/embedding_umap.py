3bdb6be26aec130feee5a15b2624858c81e69083
FranceRose <france.rose@wanadoo.fr>
2025-03-17 10:13:51 +0900

    changing threshold for periodicity

--- DISK/embedding_umap.py
+++ DISK/embedding_umap.py
@@ -80,7 +80,7 @@ def statistics_human(input_tensor, dataset_constants, device):
     coordinates_fft, _ = torch.max(torch.abs(2.0/N * torch.fft.fft(input_fft, dim=1)[:, 5:N//2]), dim=1)
     mask_low_fft = torch.all(coordinates_fft < 0.1, dim=1)
     mask_high_fft = torch.any(coordinates_fft > 2, dim=1)
-    logging.info(f'[STATISTICS] #low_fft {np.sum(mask_low_fft)} #high_fft {np.sum(mask_high_fft)}')
+    logging.info(f'[STATISTICS] #low_fft {np.sum(mask_low_fft.detach().cpu().numpy())} #high_fft {np.sum(mask_high_fft.detach().cpu().numpy())}')
 
     periodicity_cat = mask_low_fft.type(torch.float) * -1 + mask_high_fft.type(torch.float) * 1
 
@@ -132,7 +132,7 @@ def statistics_MABe(input_tensor, dataset_constants, device):
     coordinates_fft, _ = torch.max(torch.abs(2.0/N * torch.fft.fft(input_fft, dim=1)[:, 5:N//2]), dim=1)
     mask_low_fft = torch.all(coordinates_fft < 0.1, dim=1)
     mask_high_fft = torch.any(coordinates_fft > 2, dim=1)
-    logging.info(f'[STATISTICS] #low_fft {np.sum(mask_low_fft)} #high_fft {np.sum(mask_high_fft)}')
+    logging.info(f'[STATISTICS] #low_fft {np.sum(mask_low_fft.detach().cpu().numpy())} #high_fft {np.sum(mask_high_fft.detach().cpu().numpy())}')
 
     periodicity_cat = mask_low_fft.type(torch.float) * -1 + mask_high_fft.type(torch.float) * 1
 
