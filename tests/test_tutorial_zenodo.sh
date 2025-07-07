5053a4331fb6c2b5e4372ecf8f3875c9ba9a485c
FranceRose <france.rose@wanadoo.fr>
2025-07-07 15:05:53 +0200

    changing the wget links

--- tests/test_tutorial_zenodo.sh
+++ tests/test_tutorial_zenodo.sh
@@ -22,53 +22,53 @@ source activate env_impute || conda activate env_impute
 git clone https://github.com/bozeklab/DISK.git cloned-DISK-repo
 cd cloned-DISK-repo
 pip install -r DISK/requirements.txt -e . --quiet
-pip install gdown
 cd ..
 
 cd datasets
-wget -r "https://zenodo.org/records/15800035/files/Human_DISK_dataset.zip?download=1"
+wget --header 'Host: zenodo.org' --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://zenodo.org/records/15828939' --header 'Cookie: cookie_consent=essential; session=a50d7ee07c8ac6d6_6866883c.qzB_SvSWBo17flvWfCWgcfs9ISw; 5569e5a730cade8ff2b54f1e815f3670=a9c91475048ef11a7f0ba86fd6fb1964' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: same-origin' --header 'Sec-Fetch-User: ?1' 'https://zenodo.org/records/15828939/files/Human_DISK_dataset.zip?download=1' --output-document 'Human_DISK_dataset.zip'
 yes N | unzip Human_DISK_dataset.zip
 rm Human_DISK_dataset.zip
 cd ../
 
 cd models
-wget -r "https://zenodo.org/records/15800035/files/Human_transformer_proba.zip?download=1"
+wget --header 'Host: zenodo.org' --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://zenodo.org/records/15828939' --header 'Cookie: cookie_consent=essential; session=a50d7ee07c8ac6d6_6866883c.qzB_SvSWBo17flvWfCWgcfs9ISw; 5569e5a730cade8ff2b54f1e815f3670=a9c91475048ef11a7f0ba86fd6fb1964' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: same-origin' --header 'Sec-Fetch-User: ?1' 'https://zenodo.org/records/15828939/files/Human_transformer_proba.zip?download=1' --output-document 'Human_transformer_proba.zip'
 yes N | unzip Human_transformer_proba.zip
 rm Human_transformer_proba.zip
-wget -r "https://zenodo.org/records/15800035/files/Human_GRU.zip?download=1"
+wget --header 'Host: zenodo.org' --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://zenodo.org/records/15828939' --header 'Cookie: cookie_consent=essential; session=a50d7ee07c8ac6d6_6866883c.qzB_SvSWBo17flvWfCWgcfs9ISw; 5569e5a730cade8ff2b54f1e815f3670=a9c91475048ef11a7f0ba86fd6fb1964' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: same-origin' --header 'Sec-Fetch-User: ?1' 'https://zenodo.org/records/15828939/files/Human_GRU.zip?download=1' --output-document 'Human_GRU.zip'
 yes N | unzip Human_GRU.zip
 rm Human_GRU.zip
 cd ..
 
 cd datasets
-wget -r "https://zenodo.org/records/15800035/files/rat7M_raw_data.zip?download=1"
+wget --header 'Host: zenodo.org' --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://zenodo.org/records/15828939' --header 'Cookie: cookie_consent=essential; session=a50d7ee07c8ac6d6_6866883c.qzB_SvSWBo17flvWfCWgcfs9ISw; 5569e5a730cade8ff2b54f1e815f3670=a9c91475048ef11a7f0ba86fd6fb1964' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: same-origin' --header 'Sec-Fetch-User: ?1' 'https://zenodo.org/records/15828939/files/Rat7M_seq_DISK_dataset.zip?download=1' --output-document 'Rat7M_seq_DISK_dataset.zip'
 yes N | unzip Rat7M_seq_DISK_dataset.zip
 rm Rat7M_seq_DISK_dataset.zip
-wget -r "https://zenodo.org/records/15800035/files/rat7M_raw_data.zip?download=1"
+wget --header 'Host: zenodo.org' --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://zenodo.org/records/15828939' --header 'Cookie: cookie_consent=essential; session=a50d7ee07c8ac6d6_6866883c.qzB_SvSWBo17flvWfCWgcfs9ISw; 5569e5a730cade8ff2b54f1e815f3670=a9c91475048ef11a7f0ba86fd6fb1964' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: same-origin' --header 'Sec-Fetch-User: ?1' 'https://zenodo.org/records/15828939/files/rat7M_raw_data.zip?download=1' --output-document 'rat7M_raw_data.zip'
 yes N | unzip rat7M_raw_data.zip
 rm rat7M_raw_data.zip
 cd ..
 
 cd models
-wget -r "https://zenodo.org/records/15800035/files/Rat7M_transformer_proba.zip?download=1"
+wget --header 'Host: zenodo.org' --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --referer 'https://zenodo.org/records/15828939' --header 'Cookie: cookie_consent=essential; session=a50d7ee07c8ac6d6_6866883c.qzB_SvSWBo17flvWfCWgcfs9ISw; 5569e5a730cade8ff2b54f1e815f3670=a9c91475048ef11a7f0ba86fd6fb1964' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: same-origin' --header 'Sec-Fetch-User: ?1' 'https://zenodo.org/records/15828939/files/Rat7M_transformer_proba.zip?download=1' --output-document 'Rat7M_transformer_proba.zip'
 yes N | unzip Rat7M_transformer_proba.zip
+rm Rat7M_transformer_proba.zip
 cd ..
 
-python cloned-DISK-repo/tests/import_test.py
+python3 cloned-DISK-repo/tests/import_test.py
 
 cp -Rf cloned-DISK-repo/notebooks/*.yaml cloned-DISK-repo/DISK/conf/
 
-python cloned-DISK-repo/DISK/main_fillmissing.py
+python3 cloned-DISK-repo/DISK/main_fillmissing.py
 
-python cloned-DISK-repo/DISK/test_fillmissing.py
+python3 cloned-DISK-repo/DISK/test_fillmissing.py
 
 # does not work on my local computer
-python cloned-DISK-repo/DISK/embedding_umap.py --batch_size 1 --checkpoint_folder models/03-10-24_transformer_NLL --stride 60 --dataset_path .
+python3 cloned-DISK-repo/DISK/embedding_umap.py --batch_size 1 --checkpoint_folder models/03-10-24_transformer_NLL --stride 60 --dataset_path .
 
 # rm -rf datasets/Mocap_keypoints_60_stride30_new
 
 
-python cloned-DISK-repo/DISK/impute_dataset.py
+python3 cloned-DISK-repo/DISK/impute_dataset.py
 
 conda activate base
 conda remove --name env_impute --all -y
