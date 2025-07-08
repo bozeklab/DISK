f09a2d957d8f16826c63f1c39d008b80e3334641
FranceRose <france.rose@wanadoo.fr>
2025-07-08 17:00:01 +0200

    saving as svg

--- DISK/embedding_umap.py
+++ DISK/embedding_umap.py
@@ -363,12 +363,12 @@ def plot_umaps(df, all_columns, outputdir, dataset_name, suffix):
         fig.write_html(os.path.join(outputdir, f'{dataset_name}_normed_umap_colors-{label_name}_latent{suffix}.html'))
 
         plt.figure()
-        n = 5000
+        n = 8000
         if label_name == 'action_str':
             cmap = 'dark'
         else:
             cmap = "magma"
-        sns.scatterplot(df.sample(n), x='umap_x', y='umap_y', hue=label_name, s=8, palette=cmap)
+        sns.scatterplot(df.sample(n), x='umap_x', y='umap_y', hue=label_name, s=10, palette=cmap)
         plt.savefig(os.path.join(outputdir, f'{dataset_name}_normed_umap_colors-{label_name}_latent{suffix}.svg'))
 
 
