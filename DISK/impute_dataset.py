3eaf36d1e288cf67f9501f6a636f2f98fa7ac722
FranceRose <france.rose@wanadoo.fr>
2026-01-26 12:55:05 +0100

    debugging the multianimal DLC csv

--- DISK/impute_dataset.py
+++ DISK/impute_dataset.py
@@ -116,10 +116,10 @@ def save_data_original_format(data, time, file, dataset_constants, cfg_dataset, 
 
             if not np.sum(df[('scorer', 'individuals', 'bodyparts', 'coords')].isin(time_int)) == data.shape[0]:
                 print('stop')
-            logging.info(f'BEFORE -- nb of nans in data: {np.sum(np.isnan(data))}; nb of nans in df: {df[columns].isna().sum().sum()}')
+            logging.debug(f'BEFORE -- nb of nans in data: {np.sum(np.isnan(data))}; nb of nans in df: {df[columns].isna().sum().sum()}')
             df.loc[df[('scorer', 'individuals', 'bodyparts', 'coords')].isin(time_int), columns] = data.reshape((data.shape[0], -1))
             # for now replace likelihood with -1 to mark the positions where we modified the coordinate values
-            logging.info(f'AFTER -- nb of nans in data: {np.sum(np.isnan(data))}; nb of nans in df: {df[columns].isna().sum().sum()}')
+            logging.debug(f'AFTER -- nb of nans in data: {np.sum(np.isnan(data))}; nb of nans in df: {df[columns].isna().sum().sum()}')
             logging.info(f'modifying {data.shape[0]} values between indices {np.min(time_int)} and {np.max(time_int)}')
         else:
             # single animal
@@ -138,9 +138,10 @@ def save_data_original_format(data, time, file, dataset_constants, cfg_dataset, 
                     # df.loc[df.loc[:, (header, k, 'likelihood')] <= dataset_constants.DLC_LIKELIHOOD_THRESHOLD, (header, k, c)] = np.nan
                 # df.loc[df.loc[:, (header, k, 'likelihood')] <= dataset_constants.DLC_LIKELIHOOD_THRESHOLD, (header, k, 'likelihood')] = np.nan
             assert np.sum(df[('scorer', 'bodyparts', 'coords')].isin(time_int)) == data.shape[0]
-            df.loc[df[('scorer', 'bodyparts', 'coords')].isin(time_int), columns] = pd.DataFrame(columns=columns,
-                                                                                             data=data.reshape((data.shape[0], -1)))
-
+            logging.info(f'BEFORE -- nb of nans in data: {np.sum(np.isnan(data))}; nb of nans in df: {df[columns].isna().sum().sum()}')
+            df.loc[df[('scorer', 'bodyparts', 'coords')].isin(time_int), columns] = data.reshape((data.shape[0], -1))
+            logging.info(f'AFTER -- nb of nans in data: {np.sum(np.isnan(data))}; nb of nans in df: {df[columns].isna().sum().sum()}')
+            logging.info(f'modifying {data.shape[0]} values between indices {np.min(time_int)} and {np.max(time_int)}')
         # save to csv
         df.to_csv(new_file, index=False)
 
