936a5ca7c32e458714239833d9005f09bf151bed
FranceRose <france.rose@wanadoo.fr>
2026-02-02 12:37:49 +0100

    small changes in launchers

--- DISK/launchers/check_gpu.py
+++ DISK/launchers/check_gpu.py
@@ -1,14 +1,18 @@
 
 
 def main():
+    try:
+        import torch
+        is_cuda_available = torch.cuda.is_available()
+        if is_cuda_available:
+            print("✅ GPU Found!")
+        else:
+            print("⚠️ GPU not found! "
+                  "\nPlease reinstall torch linked to your Cuda wheels (details in DISK readme.")
 
-    import torch
-    is_cuda_available = torch.cuda.is_available()
-    if is_cuda_available:
-        print("GPU Found!")
-    else:
-        print("GPU not found! \nPlease reinstall torch linked to your Cuda wheels (details in DISK readme.")
-
+    except Exception:
+        print("❌ There is a problem with Torch installation."
+              "\nPlease retry installing pytorch (see README for details).")
 
 
 
--- DISK/launchers/check_install.py
+++ DISK/launchers/check_install.py
@@ -13,15 +13,20 @@ def main():
 
         test_main()
         print("✅ DISK is installed successfully.")
-    except Exception as e:
+    except Exception:
         print("❌ There is a problem with DISK installation."
               "\nPlease retry from scratch (conda env) and post a Github issue")
 
-    import torch
-    if torch.cuda.is_available():
-        print("✅ GPU is available")
-    else:
-        print("⚠️ GPU is not found.")
+    try:
+        import torch
+        if torch.cuda.is_available():
+            print("✅ GPU is available")
+        else:
+            print("⚠️ GPU is not found.")
+    except Exception:
+        print("❌ There is a problem with Torch installation."
+              "\nPlease retry installing pytorch (see README for details).")
+
 
 def cli():
     from argparse import ArgumentParser
