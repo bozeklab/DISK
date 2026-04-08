9d5b7f2db74fe8e648c6bf38bd9fb62b8f63b616
FranceRose <france.rose@wanadoo.fr>
2026-04-08 16:12:45 +0200

    structure the code better (helper function)

--- tests/cli_tests/test_cli.py
+++ tests/cli_tests/test_cli.py
@@ -1,9 +1,24 @@
 import sys
 from unittest.mock import Mock, patch
+from pathlib import Path
 
 import pytest
 
 from DISK.launchers import create_project
+
+
+def generate_test_data(root: Path, foldername: str, file_names: list[str]) -> tuple[str, list[str]]:
+
+    data_folder = root.joinpath(foldername)
+    data_files = []
+    for fname in file_names:
+        fpath = data_folder.joinpath(fname)
+        fpath.parent.mkdir(parents=True, exist_ok=True)
+        fpath.touch()
+        data_files.append(fpath)
+
+    return str(data_folder), [str(p) for p in data_files]
+
 
 def run_create_project(monkeypatch, project_path: str, file_format: str, data_files: list[str]) -> Mock:
     monkeypatch.setattr(
@@ -35,17 +50,10 @@ def test_create_project_mat_files_ok(tmp_path, monkeypatch):
     # GIVEN
     monkeypatch.chdir(tmp_path)  # set working directory to the temp directory for this test
     project_name = 'my_DISK_project'
-
-    # data/1.mat, data/2.mat
-    tmp_path.joinpath('data').mkdir()
-    data_files = []
-    for fname in ['1.mat', '2.mat']:
-        tmp_path.joinpath(f"data/{fname}").touch()
-        data_files.append(str(tmp_path.joinpath(f"data/{fname}")))
-    assert not tmp_path.joinpath(project_name).exists()
-
     file_format = 'mat_dannce'
-    project_path = str(tmp_path.joinpath('my_DISK_project'))
+    project_path = str(tmp_path.joinpath(project_name))
+    file_names = ['1.mat', '2.mat']
+    data_folder, data_files = generate_test_data(tmp_path, 'data', file_names)
 
     # WHEN
     main = run_create_project(monkeypatch, project_path, file_format, data_files)
@@ -61,18 +69,13 @@ def test_create_project_mat_folder_ok(tmp_path, monkeypatch):
     monkeypatch.chdir(tmp_path)  # set working directory to the temp directory for this test
 
     # data/1.mat, data/2.mat
-    tmp_path.joinpath('data').mkdir()
-    data_files = []
-    data_folder = str(tmp_path.joinpath('data'))
-    for fname in ['1.mat', '2.mat']:
-        tmp_path.joinpath(f"data/{fname}").touch()
-        data_files.append(str(tmp_path.joinpath(f"data/{fname}")))
-    tmp_path.joinpath(f"data/other_file.txt").touch()
-    assert not tmp_path.joinpath('my_DISK_project').exists()
-
+    file_names = ['1.mat', '2.mat']
     file_format = 'mat_dannce'
     project_name = 'my_DISK_project'
     project_path = str(tmp_path.joinpath('my_DISK_project'))
+
+    data_folder, data_files = generate_test_data(tmp_path, 'data', file_names)
+    generate_test_data(tmp_path, 'data', ['other_file.txt'])
 
     # WHEN
     main = run_create_project(monkeypatch, project_name, file_format, [data_folder])
