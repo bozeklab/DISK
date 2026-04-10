1f46a19e2f1c82f9fe39fa9042d53a27625d5a7a
FranceRose <france.rose@wanadoo.fr>
2026-04-10 12:44:35 +0200

    vanilla impute cli test

--- tests/cli_tests/test_impute_cli.py
+++ tests/cli_tests/test_impute_cli.py
@@ -7,62 +7,33 @@ import pytest
 import inspect
 from logging import Logger
 
-from DISK.launchers import train_evaluate
-
+from DISK.launchers import impute
+from test_create_project_cli import generate_test_data
+from DISK.launchers.create_project import possible_file_format_values
 
 ## UTILS
-def assert_train_evaluate_main_default_inputs(args):
-    """(project_dir, model_dir, dataset_path, dataset_name, test_dir, skeleton_graph,
-         training_seed, load_model_dir, cfg_network, training_batch_size,
-         training_epochs, learning_rate, loss_type, loss_mask, loss_factor,
-         model_scheduler_rate, model_scheduler_type, model_scheduler_steps_epoch,
-         n_cpus, print_every,
-         rerun_proba_files, indep_keypoints, merge_keypoints, suffix_proba_files,
-         add_missing_pad, viewinvariant, normalize, normalizecube, swap,
-         add_missing,
-         test_original_coordinates, test_threshold_pck,
-         n_repeat,
-         total_n_plots, plot2d_only_holes, plot3d_size, plot3d_azim,
-         logger, verbose=0)
+def assert_impute_main_default_inputs(args):
+    """project_dir, impute_dir, plot_dir, file_format, dataset_path, skeleton_graph, checkpoint, batch_size,
+         threshold_error_score, total_n_plots, plot_only_holes, missing_pad, logger, verbose=0):
+
     """
+    assert type(args['project_dir']) == str and Path(args['project_dir']).exists()
+    assert type(args['impute_dir']) == str and Path(args['impute_dir']).exists()
+    assert type(args['plot_dir']) == str and Path(args['plot_dir']).exists()
+    assert type(args['dataset_path']) == str and Path(args['dataset_path']).exists()
+    assert type(args['checkpoint']) == str and Path(args['checkpoint']).exists()
+    assert type(args['file_format']) == str and args['file_format'] in possible_file_format_values
     # skeleton_graph
     from DISK.models.graph import Graph
     assert args['skeleton_graph'] is None or isinstance(args['skeleton_graph'], Graph)
-    assert args['training_seed'] is None or type(args['training_seed']) == int
-    assert args['load_model_dir'] is None or (type(args['load_model_dir']) == str and \
-                                              (args['load_model_dir'] == '' or Path(args['load_model_dir']).exists()))
 
-    assert type(args['cfg_network']) == dict
-    assert type(args['training_batch_size']) == int and args['training_batch_size'] > 0
-    assert type(args['training_epochs']) == int and args['training_epochs'] > 0
-    assert type(args['learning_rate']) == float and args['learning_rate'] > 0
-    assert type(args['loss_type']) == str
-    assert type(args['loss_mask']) == bool
-    assert (type(args['loss_factor']) == int or type(args['loss_factor']) == float) and args['loss_factor'] > 0
-    assert type(args['model_scheduler_rate']) == float and args['model_scheduler_rate'] > 0
-    assert type(args['model_scheduler_type']) == str
-    assert type(args['model_scheduler_steps_epoch']) == int and args['model_scheduler_steps_epoch'] > 0
-    assert type(args['n_cpus']) == int and args['n_cpus'] >= 0
-    assert type(args['print_every']) == int and args['print_every'] > 0
-    assert type(args['rerun_create_proba']) == bool
-    assert type(args['indep_keypoints']) == bool
-    assert type(args['merge_keypoints']) == bool
-    assert type(args['suffix_proba_files']) == str
-    assert (len(args['add_missing_pad']) == 2 and type(args['add_missing_pad'][0]) == int \
-            and type(args['add_missing_pad'][1]) == int)
-    assert type(args['viewinvariant']) == bool
-    assert type(args['normalize']) == bool
-    assert type(args['normalizecube']) == bool
-    assert type(args['swap']) == float and args['swap'] >= 0 and args['swap'] <= 1
-    assert type(args['add_missing']) == bool
-    assert type(args['test_original_coordinates']) == bool
-    assert (type(args['test_threshold_pck']) == float and args['test_threshold_pck'] >= 0 \
-            and args['test_threshold_pck'] <= 1)
-    assert type(args['n_repeat']) == int and args['n_repeat'] > 0
+    assert type(args['batch_size']) == int and args['batch_size'] > 0
+    assert (type(args['threshold_error_score']) == float or type(args['threshold_error_score']) == int) \
+            and args['threshold_error_score'] >= 0
     assert type(args['total_n_plots']) == int and args['total_n_plots'] >= 0
-    assert type(args['plot2d_only_holes']) == bool
-    assert (type(args['plot3d_size']) == int or type(args['plot3d_size']) == float) and args['plot3d_size'] > 0
-    assert (type(args['plot3d_azim']) == int or type(args['plot3d_azim']) == float) and args['plot3d_azim'] > 0
+    assert type(args['plot_only_holes']) == bool
+    assert (len(args['missing_pad']) == 2 and type(args['missing_pad'][0]) == int \
+            and type(args['missing_pad'][1]) == int)
     assert isinstance(args['logger'], Logger)
     assert type(args['verbose']) == bool or (type(args['verbose']) == int and args['verbose'] >= 0)
 
@@ -70,7 +41,7 @@ def assert_train_evaluate_main_default_inputs(args):
 ## TESTS
 
 # @pytest.mark.skip("")
-def test_train_evaluate(tmp_path, monkeypatch):
+def test_impute(tmp_path, monkeypatch):
     # GIVEN
     monkeypatch.chdir(tmp_path)  # set working directory to the temp directory for this test
 
@@ -78,74 +49,67 @@ def test_train_evaluate(tmp_path, monkeypatch):
     dataset_name = 'dataset'
     project_path = tmp_path.joinpath(project_name)
     project_path.mkdir(exist_ok=True, parents=True)
+    model_name = 'DISK_model1'
 
-    for suffix, rerun in [['', True],
-                          ['_set_keypoints_merged', True],
-                          ['_set_keypoints', False],
-                          ]:
-        files = [
-            # f'config_project.yaml',
-            f'DISK_train/',
-            f'DISK_data/',
-            f'DISK_data/{dataset_name}/',
-            f'DISK_data/{dataset_name}/proba_missing{suffix}.csv',
-            f'DISK_data/{dataset_name}/proba_missing_length{suffix}.csv',
+    file_format = 'mat_dannce'
+    file_names = ['1.mat', '2.mat']
+    data_folder, data_files = generate_test_data(tmp_path, dataset_name, file_names)
+
+    suffix_proba_files = ''
+    files = [
+        # f'config_project.yaml',
+        f'DISK_train/{model_name}/model_epoch14',
+        f'DISK_data/{dataset_name}/',
+        f'DISK_data/{dataset_name}/proba_missing{suffix_proba_files}.csv',
+        f'DISK_data/{dataset_name}/proba_missing_length{suffix_proba_files}.csv',
+        'DISK_impute/'
+    ]
+    for file in files:
+        p = project_path.joinpath(file)
+        if file.endswith('/'):
+            p.mkdir(exist_ok=True, parents=True)
+        else:
+            p.parent.mkdir(exist_ok=True, parents=True)
+            p.touch()
+
+    config_path = project_path.joinpath('config_project.yaml')
+    config_path.write_text(dedent(
+        f"""
+        original_missing: true
+        file_format: {file_format}
+        """
+    ))
+
+    # WHEN
+    monkeypatch.setattr(
+        sys,
+        'argv',
+        [
+            'DISK-impute',
+            '--project_path', str(project_path),
+            '--dataset_name', dataset_name,
+            '--model_name', model_name
         ]
-        for file in files:
-            p = project_path.joinpath(file)
-            if file.endswith('/'):
-                p.mkdir(exist_ok=True, parents=True)
-            else:
-                p.parent.mkdir(exist_ok=True, parents=True)
-                p.touch()
+    )
 
-        config_path = project_path.joinpath('config_project.yaml')
-        config_path.write_text(dedent(
-            """
-            original_missing: true
-            """
-        ))
+    with patch('DISK.launchers.impute.main') as main:
+        impute.cli()
 
-        # WHEN
-        monkeypatch.setattr(
-            sys,
-            'argv',
-            [
-                'DISK-train',
-                '--project_path', str(project_path),
-                '--dataset_name', dataset_name,
-            ]
-        )
+    # THEN
+    sig = inspect.signature(impute.main)
+    bound_args = sig.bind(*main.call_args.args, **main.call_args.kwargs)
+    bound_args.apply_defaults()
 
-        with patch('DISK.launchers.train_evaluate.main') as main:
-            train_evaluate.cli()
+    main.assert_called_once()
 
-        # THEN
-        sig = inspect.signature(train_evaluate.main)
-        bound_args = sig.bind(*main.call_args.args, **main.call_args.kwargs)
-        bound_args.apply_defaults()
-
-        main.assert_called_once()
-
-        assert bound_args.arguments['project_dir'] == str(project_path) # project_dir
-        assert os.path.dirname(bound_args.arguments['model_dir']) == f'{project_path}/DISK_train'
-        assert os.path.dirname(bound_args.arguments['test_dir']) == bound_args.arguments['model_dir']
-        assert bound_args.arguments['dataset_path'] == str(f'{project_path}/DISK_data/{dataset_name}') # dataset_path
-        assert bound_args.arguments['dataset_name'] == dataset_name # dataset_name
-        assert bound_args.arguments['rerun_create_proba'] == rerun
-        assert_train_evaluate_main_default_inputs(bound_args.arguments)
-        ## checks what the main expects
-
-    """(project_dir, model_dir, dataset_path, dataset_name, test_dir, skeleton_graph,
-         training_seed, load_model_dir, cfg_network, training_batch_size,
-         training_epochs, learning_rate, loss_type, loss_mask, loss_factor,
-         model_scheduler_rate, model_scheduler_type, model_scheduler_steps_epoch,
-         n_cpus, print_every,
-         rerun_proba_files, indep_keypoints, merge_keypoints, suffix_proba_files,
-         add_missing_pad, viewinvariant, normalize, normalizecube, swap,
-         add_missing,
-         test_original_coordinates, test_threshold_pck,
-         n_repeat,
-         total_n_plots, plot2d_only_holes, plot3d_size, plot3d_azim,
-         logger, verbose=0)
+    assert bound_args.arguments['project_dir'] == str(project_path) # project_dir
+    assert Path(bound_args.arguments['plot_dir']).exists()
+    assert os.path.dirname(bound_args.arguments['impute_dir']) == f'{project_path}/DISK_impute'
+    assert bound_args.arguments['checkpoint'] == f'{project_path}/DISK_train/{model_name}'
+    assert bound_args.arguments['dataset_path'] == str(f'{project_path}/DISK_data/{dataset_name}') # dataset_path
+    assert bound_args.arguments['file_format'] == file_format
+    ## checks what the main expects
+    assert_impute_main_default_inputs(bound_args.arguments)
+    """project_dir, impute_dir, plot_dir, file_format, dataset_path, skeleton_graph, checkpoint, batch_size,
+         threshold_error_score, total_n_plots, plot_only_holes, missing_pad, logger, verbose=0
     """
